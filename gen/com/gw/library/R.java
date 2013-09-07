40aa7abe1f2b2c03f4bfb57e3c9b379b8fce9703
yinchuandong <yincd520@sina.com>
2013-09-07 20:52:39 +0800

    添加layout ui_login.xml

--- gen/com/gw/library/R.java
+++ gen/com/gw/library/R.java
@@ -10,6 +10,45 @@ package com.gw.library;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        /** 桔黄色 
+         */
+        public static final int azure=0x7f040016;
+        public static final int bg=0x7f040000;
+        /** 淡灰色 
+         */
+        public static final int bisque=0x7f040015;
+        public static final int black=0x7f040004;
+        /** 象牙色 
+         */
+        public static final int blanchedalmond=0x7f040012;
+        public static final int body1=0x7f040009;
+        public static final int body2=0x7f04000a;
+        public static final int divider1=0x7f04000d;
+        public static final int divider2=0x7f04000e;
+        public static final int divider3=0x7f04000f;
+        /**  橙色 背景颜色 
+         */
+        public static final int fontColor=0x7f040001;
+        /** 浅玫瑰色 
+         */
+        public static final int gainsboro=0x7f040014;
+        public static final int gray=0x7f040003;
+        public static final int ivory=0x7f040011;
+        public static final int loading=0x7f040010;
+        public static final int menu1=0x7f04000b;
+        public static final int menu2=0x7f04000c;
+        /** 白杏色 
+         */
+        public static final int mistyrose=0x7f040013;
+        /**  字体颜色 
+         */
+        public static final int red=0x7f040002;
+        public static final int sky_blue=0x7f040006;
+        public static final int sky_blue2=0x7f040007;
+        public static final int text=0x7f040008;
+        public static final int white=0x7f040005;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +56,37 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg_book=0x7f020000;
+        public static final int btn_login=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int input=0x7f020003;
+        public static final int login_bottom_repeat_bg=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090003;
+        public static final int login=0x7f090002;
+        public static final int password=0x7f090001;
+        public static final int studentNumber=0x7f090000;
     }
     public static final class layout {
         public static final int ui_launcher=0x7f030000;
+        public static final int ui_login=0x7f030001;
     }
     public static final class menu {
-        public static final int launcher=0x7f070000;
+        public static final int launcher=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int login=0x7f060004;
+        public static final int password=0x7f060006;
+        public static final int studentNumber=0x7f060005;
+        public static final int title_login=0x7f060003;
     }
     public static final class style {
         /** 
@@ -59,10 +110,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
