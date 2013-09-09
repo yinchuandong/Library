0edd34a634f007895a722eac49cad7efbbde253d
SolistWang <342916053@qq.com>
2013-09-09 09:50:15 +0800

    wjw

--- gen/com/gw/library/R.java
+++ gen/com/gw/library/R.java
@@ -11,43 +11,28 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        /** 桔黄色 
-         */
-        public static final int azure=0x7f040016;
         public static final int bg=0x7f040000;
-        /** 淡灰色 
-         */
-        public static final int bisque=0x7f040015;
-        public static final int black=0x7f040004;
-        /** 象牙色 
-         */
-        public static final int blanchedalmond=0x7f040012;
-        public static final int body2=0x7f04000a;
-        public static final int divider1=0x7f04000d;
-        public static final int divider2=0x7f04000e;
-        public static final int divider3=0x7f04000f;
+        public static final int body2=0x7f040009;
+        public static final int divider1=0x7f04000c;
+        public static final int divider2=0x7f04000d;
+        public static final int divider3=0x7f04000e;
         /**  橙色 背景颜色 
          */
         public static final int fontColor=0x7f040001;
-        /** 浅玫瑰色 
-         */
-        public static final int gainsboro=0x7f040014;
-        public static final int gray=0x7f040003;
-        public static final int grey_bg=0x7f040008;
-        public static final int ivory=0x7f040011;
-        public static final int loading=0x7f040010;
-        public static final int menu1=0x7f04000b;
-        public static final int menu2=0x7f04000c;
-        /** 白杏色 
-         */
-        public static final int mistyrose=0x7f040013;
         /**  字体颜色 
          */
-        public static final int red=0x7f040002;
-        public static final int sky_blue=0x7f040006;
-        public static final int sky_blue2=0x7f040007;
-        public static final int tab_bg=0x7f040009;
-        public static final int white=0x7f040005;
+        public static final int gray=0x7f040002;
+        public static final int grey_bg=0x7f040007;
+        public static final int loading=0x7f04000f;
+        public static final int menu1=0x7f04000a;
+        public static final int menu2=0x7f04000b;
+        public static final int red=0x7f040003;
+        public static final int sky_blue=0x7f040005;
+        public static final int sky_blue2=0x7f040006;
+        public static final int tab_bg=0x7f040008;
+        /**  remind部分的文字提醒 
+         */
+        public static final int white=0x7f040004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -86,27 +71,19 @@ public final class R {
         public static final int pull_to_refresh_header_background=0x7f020014;
     }
     public static final class id {
-        public static final int action_settings=0x7f090017;
+        public static final int action_settings=0x7f09000f;
         public static final int head_arrowImageView=0x7f090005;
         public static final int head_contentLayout=0x7f090004;
         public static final int head_lastUpdatedTextView=0x7f090008;
         public static final int head_progressBar=0x7f090006;
         public static final int head_tipsTextView=0x7f090007;
-        public static final int layout_main_header=0x7f090010;
-        public static final int layout_main_tab=0x7f090011;
-        public static final int login=0x7f090014;
+        public static final int layout_main_header=0x7f09000a;
+        public static final int layout_main_tab=0x7f09000b;
+        public static final int login=0x7f09000e;
         public static final int main_title=0x7f090000;
-        public static final int password=0x7f090013;
-        public static final int remind_book_return_data=0x7f09000c;
-        public static final int remind_book_title=0x7f09000b;
-        public static final int remind_cancel=0x7f09000e;
-        public static final int remind_data=0x7f09000a;
-        public static final int remind_list=0x7f090016;
-        public static final int remind_main=0x7f090009;
-        public static final int remind_menu=0x7f09000d;
-        public static final int remind_ok=0x7f09000f;
-        public static final int remind_setting=0x7f090015;
-        public static final int studentNumber=0x7f090012;
+        public static final int password=0x7f09000d;
+        public static final int r_number=0x7f090009;
+        public static final int studentNumber=0x7f09000c;
         public static final int tab_history=0x7f090003;
         public static final int tab_loan=0x7f090002;
         public static final int tab_recommend=0x7f090001;
@@ -133,6 +110,7 @@ public final class R {
         public static final int login=0x7f060009;
         public static final int password=0x7f06000b;
         public static final int recommend=0x7f060002;
+        public static final int remain=0x7f06000c;
         public static final int remind=0x7f060001;
         public static final int remind_cancel=0x7f060003;
         public static final int remind_ok=0x7f060004;
