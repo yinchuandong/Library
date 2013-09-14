186f0915c38078ec98b4d9374e19d243da155bde
kelink <1030041097@qq.com>
2013-09-14 17:27:16 +0800

    luo

--- src/com/gw/library/service/NotifyService.java
+++ src/com/gw/library/service/NotifyService.java
@@ -2,6 +2,8 @@ package com.gw.library.service;
 
 import com.gw.library.base.BaseService;
 
-public class NotifyService extends BaseService{
+public class NotifyService extends BaseService {
+
+	// 轮询刷新数据
 
 }
